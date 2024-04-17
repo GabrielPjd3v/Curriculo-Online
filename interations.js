7347f09e6c7d92b6a6341c38e11e149242082720
GabrielPjd3v <gabriel.contato2001@gmail.com>
2024-04-17 14:46:50 -0300

    responsividades e menu hamburguer

---
+++ interations.js
@@ -0,0 +1,10 @@
+const menudiv = document.getElementById('menu-mobile')
+const btnAnimar = ducument.getElementById('amburguer')
+
+menudiv.addEventListener('click', amburgui)
+
+function amburgui(){
+    menudiv.classList.toggle('abrir') 
+      
+}
+
